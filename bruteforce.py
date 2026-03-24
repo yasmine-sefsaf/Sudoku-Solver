29a73fcf17bdd70d84e8deefb24fb5af7e4dc182
Michel <michelrostain@laplateforme.io>
2026-03-24 12:12:15 +0100

    premier fichiers

---
+++ bruteforce.py
@@ -0,0 +1,58 @@
+
+def trouver_case_vide(grille):
+    for i in range(81):
+        if grille[i] == 0:
+            return i
+    return None
+
+def chiffre_absent_ligne(grille, ligne, chiffre):
+    # Une ligne = 9 cases consécutives
+    # La ligne 0 commence à l'index  0, la ligne 1 à l'index 9, etc.
+    debut = ligne*9
+    for i in range(debut, debut+9):
+        if grille[i] == chiffre:
+            # return False — retourné à l'intérieur de la boucle, dès qu'une case de la ligne contient déjà le chiffre cherché. On sort immédiatement, inutile de continuer à vérifier.
+            return False
+    # return True — retourné après la boucle, uniquement si on a parcouru les 9 cases sans jamais trouver le chiffre. Il est donc absent de la ligne.
+    return True
+
+def chiffre_absent_colonne(grille, colonne, chiffre):
+    #  Toutes les 9 cases, espacées de 9
+    for i in range(colonne, 81, 9):
+        if grille [i]== chiffre:
+            return False
+    return True
+
+def chiffre_absent_bloc(grille, case, chiffre):
+    # Trouver le coin supérieur gauche du bloc
+    ligne = (case // 9 // 3) * 3
+    colonne = (case % 9// 3) * 3
+
+    for l in range(ligne, ligne+3):
+        for c in range(colonne, colonne+3):
+            if grille[l*9+c] == chiffre:
+                return False
+    return True
+
+def est_valide(grille, case, chiffre):
+    ligne = case // 9
+    colonne = case % 9
+    return (chiffre_absent_ligne(grille, ligne, chiffre) and chiffre_absent_colonne(grille, colonne, chiffre) and chiffre_absent_bloc(grille, case, chiffre))
+
+
+
+######### Vérification de la force brute
+def verif_grille_bruteforce(grille):
+    for case in range(81):
+        chiffre = grille[case]
+
+        # Si une case a zéro, grille fausse.
+        if chiffre == 0:
+            return False
+        
+        ligne = case // 9
+        colonne = case % 9
+
+        if not (chiffre_absent_ligne(grille, ligne, chiffre) and chiffre_absent_colonne(grille, colonne, chiffre) and chiffre_absent_bloc(grille, case, chiffre)):
+            return False
+    return True # Toutes les cases validée : grille résolue
---
+++ sudoku.py
@@ -0,0 +1,6 @@
+from bruteforce import verif_grille_bruteforce
+
+class SudokuGrid :
+    def __init__(self):
+        pass
+
